52cc6985c9d46a37dc6550707a30354441881265
Berchez Adrian Victor <93824195+ynsandy@users.noreply.github.com>
2022-06-13 14:38:27 +0300

    Delete zte directory

--- zte/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $cnx = mysqli_connect("localhost","root","","new.scrap.workshop");  //  user: root, fara parola!
-  // Conectare reusita?
-  if (mysqli_connect_errno()) {
-     die("Conectare la MySQL nereusita: " . mysqli_connect_error());
-  }
-  // Impun setul de caractere utf8
-  mysqli_set_charset($cnx,"utf8");
-?>
